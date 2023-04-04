ed8f813e9bcf63484e7318f2817de13284b1045d
sujanpatro-surya <sujan.kp@surya-digital.com>
2023-04-04 12:13:51 +0530

    made private variables from public

--- lib/screens/homepage/blog_body.dart
+++ lib/screens/homepage/blog_body.dart
@@ -8,50 +8,52 @@ class MyBlogBody extends StatefulWidget {
 }
 
 class _MyBlogBodyState extends State<MyBlogBody> {
-  String blogText = 'Welcome to my blog';
-
+  String _blogText = 'Welcome to my blog';
+  final String _indiaBlogText = 'This is the country India. This is a beautiful country. It is a secular country, not a developed country.';
+  final String _canadaBlogText = 'This is the country Canada. This is not USA. It is a separate country by itself.';
+  final String _chinaBlogText = 'This is the country China. It is very different from other countries of the world. It manages its own matters by itself.';
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(blogText, style: const TextStyle(fontSize: 20),),
-            ElevatedButton(
-              onPressed: _canadaBlog,
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.green.shade300),
-              child: const Text('Canada'),
-            ),
-            ElevatedButton(
-              onPressed: _indiaBlog,
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.red.shade300),
-              child: const Text('India'),
-            ),
-            ElevatedButton(
-              onPressed: _chinaBlog,
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.yellow.shade300),
-              child: const Text('China'),
-            )
-          ]
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(_blogText, style: const TextStyle(fontSize: 20)),
+          ElevatedButton(
+            onPressed: _canadaBlog,
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.green.shade300),
+            child: const Text('Canada')
+          ),
+          ElevatedButton(
+            onPressed: _indiaBlog,
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.red.shade300),
+            child: const Text('India')
+          ),
+          ElevatedButton(
+            onPressed: _chinaBlog,
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.yellow.shade300),
+            child: const Text('China')
+          )
+        ]
       ),
     );
   }
 
   void _canadaBlog() {
     setState(() {
-      blogText = 'This is the country Canada. This is not USA. It is a separate country by itself.';
+      _blogText = _canadaBlogText;
     });
   }
 
   void _indiaBlog() {
     setState(() {
-      blogText = 'This is the country India. This is a beautiful country. It is a secular country, not a developed country.';
+      _blogText = _indiaBlogText;
     });
   }
 
   void _chinaBlog() {
     setState(() {
-      blogText = 'This is the country China. It is very different from other countries of the world. It manages its own matters by itself.';
+      _blogText = _chinaBlogText;
     });
   }
 }
